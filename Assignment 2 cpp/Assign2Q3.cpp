ee6dc5d3c3ef8e5f60226c1e505fade702acdd8d
Deesh <deveshsharma372002@gmail.com>
2024-03-08 04:18:40 -0800

    Assignment 2 Q1,2,3 compleated

--- Assignment 2 cpp/Assign2Q3.cpp
+++ Assignment 2 cpp/Assign2Q3.cpp
@@ -24,11 +24,55 @@ class Student{
         cin>>name;
         cout<<"Enter Mark's"<<endl;
         cin>>marks;
+    }
+    void printStudentONConsole(){
+        cout<<"Roll Number:"<<rollNum<<endl;
+        cout<<"Name: "<<name<<endl;
+        cout<<"Marks: "<<marks<<endl;
 
-    };
+    }
+    void acceptStudentFromConsole(){
+        intitStudent();
+    }
 
 
 };
     
+}
+
+int main(){
+    // using namespace NStudent;
+    NStudent::Student S;
+    int choose;
+    do
+    {
+        cout << "\nMenu:\n";
+        cout << "1. Initialize Student\n";
+        cout << "2. Print Student Details\n";
+        cout << "3. Accept Student Details\n";
+        cout << "4. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choose;
+
+        switch (choose)
+        {
+        case 1:
+        S.intitStudent();
+            break;
+            case 2:
+            S.printStudentONConsole();
+            break;
+            case 3:
+            S.acceptStudentFromConsole();
+            break;
+        
+        default:
+        cout<<"Invalid Choice"<<endl;
+            break;
+        }
+        
+    } while (choose !=4);
+    
+return 0;
 }
 
